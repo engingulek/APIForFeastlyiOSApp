22e8a8b8ab8e9db1e24ccbad81d2e8f0153a0741
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2024-09-28 16:36:22 +0300

    Created Default Project

---
+++ apiForFeastly/src/test/java/com/example/apiForFeastly/ApiForFeastlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiForFeastly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForFeastlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
